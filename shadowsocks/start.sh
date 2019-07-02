d530fb188a780d923917b3c06da74563bd24c9e0
woider <sungongpcd373@gmail.com>
2019-07-02 21:48:38 +0800

    fff

--- shadowsocks/start.sh
+++ shadowsocks/start.sh
@@ -1,6 +1,6 @@
 sudo chmod 777 /etc
 
-cp ../config.json /etc/shadowsocks.json
+sudo cp ../config.json /etc/shadowsocks.json
 
 sudo chmod 777 /etc/shadowsocks.json
 
@@ -8,11 +8,13 @@ sudo chmod 777 /etc/shadowsocks.json
 sudo apt-get install build-essential
 
 
+
+wget https://download.libsodium.org/libsodium/releases/LATEST.tar.gz
+tar zxf LATEST.tar.gz
 cd libsodium*
 ./configure
 sudo make && sudo make install
 echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf
 ldconfig
-
 
 
